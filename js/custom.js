7477ef15c428f47fc62119cc859a59187c0ff916
Bianca Currie <270021658@bun2-206-16.acg.ac.nz>
2021-04-13 12:06:13 +1200

    code cleanup

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
 // custom javascript
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<
+script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js" > < /script>
